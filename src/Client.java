4b4b17b09345fbcfbac3a37b7ca56ddff8d53a43
MrRuulici <92568296+MrRuulici@users.noreply.github.com>
2022-03-17 18:36:24 +0200

    Add files via upload

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println(Suma.suma(2,2,4,3));
+	}
+
+}
---
+++ src/Suma.java
@@ -0,0 +1,22 @@
+
+public class Suma {
+
+	private int a, b, c, d;
+
+	public static int suma(int a, int b) {
+		return a+b;
+	}
+
+
+	public static int suma(int a, int b, int c) {
+		return suma(a,b) + c;
+	}
+
+	
+	public static int suma(int a, int b, int c, int d) {
+		return suma(a,b,c) + d;
+	}
+	
+	
+	
+}
